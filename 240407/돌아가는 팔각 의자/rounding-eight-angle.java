476d1690f3c7acf3388d6fa7a64247ff25781669
elle6044 <123047819+elle6044@users.noreply.github.com>
2024-04-07 21:12:19 +0900

    Title: 돌아가는 팔각 의자, Time: 111ms, Memory: 8MB, Status: Passed - CodeTree

--- 240407/돌아가는 팔각 의자/rounding-eight-angle.java
+++ 240407/돌아가는 팔각 의자/rounding-eight-angle.java
@@ -62,8 +62,8 @@ public class Main {
 		}
 		
 		int sum=0;
-		for(int i=1;i<=4;i++) {
-			sum+=map[i-1][0]*i;
+		for(int i=0;i<4;i++) {
+			sum+=map[i][0]*Math.pow(2, i);
 		}
 		bw.write(sum+"");
 		bw.close();
