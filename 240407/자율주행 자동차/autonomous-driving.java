59d559ee38971e8ba0078663ec051fd120f45a35
elle6044 <123047819+elle6044@users.noreply.github.com>
2024-04-07 17:42:46 +0900

    Title: 자율주행 자동차, Time: 93ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240407/자율주행 자동차/autonomous-driving.java
@@ -0,0 +1,80 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+
+	static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
+	
+	static int N,M;
+	static int[][] map;
+	static boolean[][] v;
+	
+	static int[] dr= {-1,0,1,0};
+	static int[] dc= {0,1,0,-1};
+	public static void main(String[] args) throws Exception{
+		st=new StringTokenizer(br.readLine());
+		N=Integer.parseInt(st.nextToken());
+		M=Integer.parseInt(st.nextToken());
+		
+		st=new StringTokenizer(br.readLine());
+		int x=Integer.parseInt(st.nextToken());
+		int y=Integer.parseInt(st.nextToken());
+		int d=Integer.parseInt(st.nextToken());
+		
+		map=new int[N][M];
+		for(int i=0;i<N;i++) {
+			st=new StringTokenizer(br.readLine());
+			for(int j=0;j<M;j++) {
+				map[i][j]=Integer.parseInt(st.nextToken());
+			}
+		}
+		v=new boolean[N][M];
+		
+		
+		bfs(x,y,d);
+		
+		bw.write(answer+"");
+		bw.close();
+	}
+	static int answer=1;
+	
+	public static void bfs(int r, int c, int dir) {
+		Queue<Point> q=new ArrayDeque();
+		q.offer(new Point(r,c,dir));
+		v[r][c]=true;
+		
+		while(!q.isEmpty()) {
+			Point p=q.poll();
+			L:for(int d=1;d<=4;d++) {
+				int nd=(p.d+3*d)%4;
+				int nr=p.r+dr[nd];
+				int nc=p.c+dc[nd];
+				if(nr>=0&&nr<N&&nc>=0&&nc<M&&map[nr][nc]==0) {
+					if(!v[nr][nc]) {
+						v[nr][nc]=true;
+						q.offer(new Point(nr,nc,nd));
+						answer++;
+						continue L;
+					}
+				}
+			}
+			int nd=(p.d+2)%4;
+			int nr=p.r+dr[nd];
+			int nc=p.c+dc[nd];
+			if(nr>=0&&nr<N&&nc>=0&&nc<M&&map[nr][nc]==0) {
+				q.offer(new Point(nr,nc,p.d));
+			}
+		}
+	}
+	
+	static class Point{
+		int r,c,d;
+		public Point(int r, int c, int d) {
+			this.r=r;
+			this.c=c;
+			this.d=d;
+		}
+	}
+
+}
